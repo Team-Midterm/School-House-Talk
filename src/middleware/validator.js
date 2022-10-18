1662f1b8eebc082a612c6493507c4d5d03c9150a
Isaiah Keller <isaiah.keller@gmail.com>
2022-10-18 17:19:54 +0000

    added notes to validator

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const validator = (req, res, next) => {
+  // need to look into adding a more specific information on the exact part of the model or query is missing
   req.query.name ? next() : next('**** name required ****');
 };
 
