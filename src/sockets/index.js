77f4fbaebebe56b8d94e33f2fffd2861eb682207
LUIS I ROSALES JR <luis.rosalesjr01@gmail.com>
2022-10-19 23:47:40 +0000

    socket foundation up

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -5,19 +5,27 @@ const SOCKETPORT = process.env.SOCKETPORT || 3004;
 const server = new Server(SOCKETPORT);
 const messages = server.of('/messages');
 
-// const { io } = require('socket.io-client');
+
 
 
 
 //subscribe and publish
 ///server hub some sort of emit to 
-console.log('Listening SOCKET PORT');
+console.log('Listening SOCKET PORT', SOCKETPORT);
 
 
-server.on('connection', (socket) => {
+messages.on('connection', (socket) => {
   console.log('SOCKET CONNECTED', socket.id);
+  socket.onAny((event, payload) => {
+    const date = new Date();
+    const time = date.toTimeString();
+    console.log('EVENT', {event, time, payload});
+  });
   socket.on('GAME-ALERT', (payload) => {
     console.log('PROOF OF LIFE', payload);
   });
+
+  // socket.broadcast.emit('')
 });
 
+module.exports = SOCKETPORT;
