780371834e97433b186434088ac4f9eb67a75fda
LUIS I ROSALES JR <luis.rosalesjr01@gmail.com>
2022-10-19 02:59:40 +0000

    refactored tests for 71% coverage

--- __tests__/auth-tests/auth.test.js
+++ __tests__/auth-tests/auth.test.js
@@ -3,6 +3,10 @@
 const base64 = require('base-64');
 const middleware = require('../../src/auth/basic');
 const { sequelize, users } = require('../../src/models');
+const bearer = require('../../src/auth/bearer');
+const jwt = require('jsonwebtoken');
+
+
 
 let testUser = {
   admin: {username: 'test-admin', password: 'pass'},
@@ -17,50 +21,70 @@ afterAll(async () => {
   await sequelize.drop();
 });
 
-describe('Auth Middleware', () => {
+// Mock the express req/res/next that we need for each middleware call
+const req = {};
+const res = {
+  status: jest.fn(() => res),
+  send: jest.fn(() => res),
+};
+const next = jest.fn();
 
-  // admin:password: YWRtaW46cGFzc3dvcmQ=
-  // admin:foo: YWRtaW46Zm9v
+describe('user authentication', () => {
 
-  // Mock the express req/res/next that we need for each middleware call
+  it('fails a login for a user (admin) with the incorrect basic credentials', () => {
+    const basicAuthString = base64.encode('username:password');
+
+    // Change the request to match this test case
+    req.headers = {
+      authorization: `Basic ${basicAuthString}`,
+    };
+
+    return middleware(req, res, next)
+      .then(() => {
+        expect(next).not.toHaveBeenCalled();
+        expect(res.status).toHaveBeenCalledWith(403);
+      });
+
+  });
+
+  it('logs in an admin user with the right credentials', () => {
+    let basicAuthString = base64.encode(`${testUser.admin.username}:${testUser.admin.password}`);
+
+    // Change the request to match this test case
+    req.headers = {
+      authorization: `Basic ${basicAuthString}`,
+    };
+
+    return middleware(req, res, next)
+      .then(() => {
+        expect(next).toHaveBeenCalledWith();
+      });
+
+  });
+});
+
+
+describe('user authentication', () => {
   const req = {};
   const res = {
     status: jest.fn(() => res),
     send: jest.fn(() => res),
+    json: jest.fn(() => res),
   };
   const next = jest.fn();
 
-  describe('user authentication', () => {
 
-    it('fails a login for a user (admin) with the incorrect basic credentials', () => {
-      const basicAuthString = base64.encode('username:password');
+  it('logs in a user with a proper token', () => {
 
-      // Change the request to match this test case
-      req.headers = {
-        authorization: `Basic ${basicAuthString}`,
-      };
+    const user = { username: 'admin' };
+    const token = jwt.sign(user, process.env.SECRET);
+    req.headers = {
+      authorization: `Bearer ${token}`,
+    };
 
-      return middleware(req, res, next)
-        .then(() => {
-          expect(next).not.toHaveBeenCalled();
-          expect(res.status).toHaveBeenCalledWith(403);
-        });
-
-    });
-
-    it('logs in an admin user with the right credentials', () => {
-      let basicAuthString = base64.encode(`${testUser.admin.username}:${testUser.admin.password}`);
-
-      // Change the request to match this test case
-      req.headers = {
-        authorization: `Basic ${basicAuthString}`,
-      };
-
-      return middleware(req, res, next)
-        .then(() => {
-          expect(next).toHaveBeenCalledWith();
-        });
-
-    });
+    return bearer(req, res, next)
+      .then(() => {
+        expect(next).toHaveBeenCalledWith('Login Invalid');
+      });
   });
 });
--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const authRouter = require('./routes/auth-route');
 const PORT = process.env.PORT || 3002;
 const app = express();
 const studentRouter = require('./routes/student');
+const errorHandler = require('./middleware/500');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
@@ -15,6 +16,12 @@ app.use(studentRouter);
 app.get ('/', (req, res, next) => {
   res.status(200).send('Welcome to the School-House-Talk server');
 });
+
+app.get('/bad', (req, res, next) => {
+  next('this is a bad route');
+});
+
+app.use(errorHandler);
 
 function start () {
   app.listen(PORT, () => console.log(`listening on PORT: ${PORT}`));
